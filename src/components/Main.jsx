dd8e0996a6c3998503342d1e879148ce20baee8c
ErickPaulSweeney <erickpaulsweeney@gmail.com>
2022-07-18 14:42:11 +0800

    Set event listener to images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,7 +7,6 @@ function Main(props) {
     let day = new Date().getDate().toString();
     let hour = new Date().getHours().toString();
     let minute = new Date().getMinutes().toString();
-    // console.log(year, month, day, hour, minute)
     
     let pencil = "https://images.unsplash.com/photo-1513542789411-b6a5d4f31634?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&w=1920&q=80";
     let wavy = "https://images.unsplash.com/photo-1607893351349-0cfa621476ed?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80";
@@ -119,16 +118,18 @@ function Main(props) {
                                         setPicture(pencil);
                                         setBorder5(null);
                                         setError('none');
-                                    }} />
-                                <img className="option pencil-img" src={pencil} alt="Pencil" />
+                                    }} 
+                                    checked={picture === pencil} />
+                                <img className="option pencil-img" src={pencil} alt="Pencil" onClick={() => setPicture(pencil)} />
                                 <input type="radio" id="wavy" name="picture" value={wavy}
                                     style={{ outline: border5 }}
                                     onChange={() => {
                                         setPicture(wavy);
                                         setBorder5(null);
                                         setError('none');
-                                    }} />
-                                <img className="option wavy-img" src={wavy} alt="Wavy Blue" />
+                                    }}
+                                    checked={picture === wavy} />
+                                <img className="option wavy-img" src={wavy} alt="Wavy Blue" onClick={() => setPicture(wavy)} />
                             </div>
 
                             <button type="submit" className="submit-button">Start Timer</button>
@@ -142,7 +143,6 @@ function Main(props) {
                     <div className="finished-headline">{main}</div>
                     <div className="finished-secondary">{secondary}</div>
                     <div className="finished-subtext">{subtext}</div>
-                    {/* <div className="container-countdown">{date}</div> */}
                     <CountdownTimer targetDate={date} />
                     <div className="finished-button">{button}</div>
                 </div>
